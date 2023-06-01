e454cd8cb41b0c4b9fdca9fbaf6a859833ca57e5
ranjith <ranjithkumarcena@gmail.com>
2023-06-01 16:37:48 -0400

    Project setup done

--- app/src/main/java/com/android/composepokemon/MainActivity.kt
+++ app/src/main/java/com/android/composepokemon/MainActivity.kt
@@ -8,16 +8,60 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.android.composepokemon.ui.theme.ComposePokemonTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePokemonTheme {
 
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemon_list_screen"
+                ){
+
+                    composable("pokemon_list_screen"){
+
+
+                    }
+
+                    composable("pokemon_details_screen/{dominantColor}/{pokemonName}",
+                    arguments = listOf(
+                        navArgument("dominantColor"){
+                            type = NavType.IntType
+                        },
+                        navArgument("pokemonName"){
+                            type = NavType.StringType
+                        }
+                        )
+                    ){
+
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White // if the color parameter is empty
+
+                        }
+
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+
+
+                    }
+
+                }
 
             }
         }
